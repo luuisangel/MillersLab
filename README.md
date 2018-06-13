72baae7f2c261458a6382094e50741da3336e18e
Luis <luis.quinones21@upr.edu>
2018-06-12 23:52:06 -0400

    results fully functional

---
+++ README.md
@@ -0,0 +1,26 @@
+# Proyecto
+
+Miller Lab Projects Database (http://ada.uprrp.edu/~luis.quinones21/MillersLab/home.php)
+
+### Pre requisitos
+
+Para la completa funcionalidad de este sistema se necesita un navegador que pueda correr JavaScript y HTML5 
+
+```
+Google Chrome
+Safari
+Mozilla
+```
+
+## Descripción
+
+Este sistema de manejo de datos provee una interfaz web para mantener los resultados de los estudiantes que realizan investigación en el laboratorio del Dr. Mark Miller del Instituto de Neurobiología de la Universidad de Puerto Rico, Recinto de Ciencias Médicas.
+
+## Funcionalidades
+
+La página *HOME* provee una lista de los proyectos que se trabajan en el laboratorio. Estos proyectos pueden ser presionados para expandir una lista con las técnicas utilizadas para obtener los resultados correspondientes de cada proyecto. Estas técnicas también pueden ser presionadas para expandir una lista de imágenes que corresponden a resultados.
+
+## Autor
+
+* **Luis A. Quiñones** 
+
--- create tables/lab.sql
+++ create tables/lab.sql
@@ -2,33 +2,35 @@ create table Students(
 	sid integer,
 	sname char(30) not null,
 	email char(30),
-	primary key (sid) ON UPDATE CASCADE ON DELETE CASCADE
+	primary key (sid) 
 );
 
 create table Antibodies(
 	aid char(20),
+	pid integer,
 	`type` char(20),
 	host char(20),
 	fluorescence char(20),
-	primary key (aid) ON UPDATE CASCADE ON DELETE CASCADE
+	primary key (aid) 
+	foreign key (pid) references Projects (pid) ON UPDATE CASCADE ON DELETE CASCADE,
 );
 
 create table ResultsPhotos(
 	`path` varchar(255),
 	`date` date not null,
-	primary key (`path`) ON UPDATE CASCADE ON DELETE CASCADE
+	primary key (`path`) 
 );
 
 create table Projects(
 	pid integer,
 	pname char(50) not null,
-	primary key (pid) ON UPDATE CASCADE ON DELETE CASCADE
+	primary key (pid) 
 );
 
 create table Techniques(
 	technique char(20),
-	primary key (technique) ON UPDATE CASCADE ON DELETE CASCADE
-)
+	primary key (technique) 
+);
 
 create table Results(
 	`path` varchar(255) not null,
@@ -37,10 +39,10 @@ create table Results(
 	sid integer not null,
 	technique char(20),
 	primary key (`path`),
-	foreign key (pid) references Projects (pid),
-	foreign key (aid) references Antibodies (aid),
-	foreign key (sid) references Students (sid),
-	foreign key (`path`) references ResultsPhotos (`path`), 
+	foreign key (pid) references Projects (pid) ON UPDATE CASCADE ON DELETE CASCADE, 
+	foreign key (aid) references Antibodies (aid) ON UPDATE CASCADE ON DELETE CASCADE,
+	foreign key (sid) references Students (sid) ON UPDATE CASCADE ON DELETE CASCADE,
+	foreign key (`path`) references ResultsPhotos (`path`) ON UPDATE CASCADE ON DELETE CASCADE, 
 	foreign key (technique) references Techniques (technique)
 );
 
@@ -48,7 +50,7 @@ create table Stud_Proj(
 	pid integer,
 	sid integer,
 	primary key (sid,pid),
-	foreign key (sid) references Students (sid),
-	foreign key (pid) references Projects (pid)
+	foreign key (sid) references Students (sid) ON UPDATE CASCADE ON DELETE CASCADE,
+	foreign key (pid) references Projects (pid) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
